4ea48632b25282c2ae9ad931baa9927fff4eab67
architecte <samuel.adoni-willybiro@epita.fr>
2026-04-15 15:41:23 +0200

    feat: enable same site attribute

--- src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/configuration/SecurityConfiguration.java
+++ src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/configuration/SecurityConfiguration.java
@@ -27,12 +27,12 @@ public class SecurityConfiguration {
         return http
             .authorizeHttpRequests(auth -> auth
                 // Step 4a: add access control
-                // ...
+                .requestMatchers("/dashboard").hasRole("ADMIN")
                 // Step 4a: end
                 .anyRequest().permitAll()
             )
             // Step 4b: Add login form
-            // ...
+            .formLogin(withDefaults())
             // Step 4b: End of login form configuration
             
             .csrf((csrf) -> csrf
@@ -45,9 +45,29 @@ public class SecurityConfiguration {
             )
             .build();
     }
-
     // Step 3: add InMemoryUserDetailsManager
-    // ...
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("password")
+            .roles("USER")
+            .build();
+
+        UserDetails administrator = User.builder()
+            .username("admin")
+            .password("{bcrypt}$2a$10$tz40qyDsPbN4r6/YrmZX4.fDF/aeZ0wsBU8BElWfeq55aNM6HLLGG")
+            .roles("ADMIN")
+            .build();
+
+        UserDetails guest = User.builder()
+                .username("guest")
+                .password("{bcrypt}$2a$10$A1CVRhBfmDjbqMeutfuvgOTIGpGu4qOYu8uNgkXAaCv2xCrd0MVyu")
+                .roles("GUEST")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, administrator, guest);
+    }
     // Step 3: end
 
 }
